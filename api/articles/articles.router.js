67df4f9966fcd8aebfaee73981d4ad8364a3785e
ARevault <antonin.revault@outlook.fr>
2023-03-22 16:19:14 +0100

    Erreur 404 parenthèses Routes

--- api/articles/articles.router.js
+++ api/articles/articles.router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get("/", articlesController.getAll);
 router.get("/:id", articlesController.getById);
-router.post(("/", articlesController.create));
-router.put(("/:id", articlesController.update));
-router.delete(("/:id", articlesController.delete));
+router.post("/", articlesController.create);
+router.put("/:id", articlesController.update);
+router.delete("/:id", articlesController.delete);
 
 module.exports = router;
